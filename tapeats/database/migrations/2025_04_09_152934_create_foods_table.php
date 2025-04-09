62093ea50b1c89d0159d9fefb31ade8a7a4a00b6
James Anderson <jamesandrn@gmail.com>
2025-04-09 23:21:26 +0700

    Membuat model,migration,relationship

---
+++ tapeats/database/migrations/2025_04_09_152934_create_foods_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('foods', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('image');
+            $table->string('price');
+            $table->string('price_afterdiscount')->nullable();;#nullable(kolomnya boleh kosong)
+            $table->string('percent')->nullable();;
+            $table->string('is_promo')->nullable();;
+            $table->string('categories_id')->constrained('categories')->cascadeOnDelete();#Refer ke tabel categories
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('foods');
+    }
+};
