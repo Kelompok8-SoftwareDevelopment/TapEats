89c5b9756ce1792994f02775a0340bf05eb5359e
James Anderson <jamesandrn@gmail.com>
2025-05-02 22:04:32 +0700

    test

---
+++ tapeats/app/Filament/Resources/BarcodesResource/Pages/CreateBarcodes.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\BarcodesResource\Pages;
+
+use App\Filament\Resources\BarcodesResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateBarcodes extends CreateRecord
+{
+    protected static string $resource = BarcodesResource::class;
+}
