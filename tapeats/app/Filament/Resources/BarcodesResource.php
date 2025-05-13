e16e4f33bbba7f9eb58b135ed760880380f55f8f
James Anderson <jamesandrn@gmail.com>
2025-05-13 15:53:32 +0700

    Update Barcodes
    - Bisa bikin barcode baru
    - Membuat tampilan list di menu Barcodes pada dashboard admin

--- tapeats/app/Filament/Resources/BarcodesResource.php
+++ tapeats/app/Filament/Resources/BarcodesResource.php
@@ -25,16 +25,17 @@ class BarcodesResource extends Resource
             ->schema([
                 Forms\Components\TextInput::make('table_number')
                     ->required()
-                    ->maxLength(255),
+                    ->default(fn() => strtoupper(chr(rand(65,90)) . rand(1000, 9999))),
                 Forms\Components\FileUpload::make('image')
                     ->image()
-                    ->required(),
+                    ->required()
+                    ->columnSpanFull(),
+                Forms\Components\Select::make('users_id')
+                    ->required()
+                    ->relationship('users', 'name'),
                 Forms\Components\TextInput::make('qr_value')
                     ->required()
                     ->maxLength(255),
-                Forms\Components\TextInput::make('users_id')
-                    ->required()
-                    ->numeric(),
             ]);
     }
 
@@ -44,12 +45,11 @@ class BarcodesResource extends Resource
             ->columns([
                 Tables\Columns\TextColumn::make('table_number')
                     ->searchable(),
-                Tables\Columns\ImageColumn::make('image'),
+                // Tables\Columns\ImageColumn::make('image'),
                 Tables\Columns\TextColumn::make('qr_value')
                     ->searchable(),
-                Tables\Columns\TextColumn::make('users_id')
-                    ->numeric()
-                    ->sortable(),
+                Tables\Columns\TextColumn::make('users.name')
+                    ->searchable(),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
@@ -63,7 +63,18 @@ class BarcodesResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                Tables\Actions\Action::make('download')
+                ->label('Download QR Code')
+                ->icon('heroicon-o-arrow-down-tray')
+                ->action(function ($record) {
+                    $filePath = storage_path('app/public/' . $record->image);
+                    if (file_exists($filePath)) {
+                        return response()->download($filePath);
+                    }
+                    
+                    session()->flash('error', 'QR code image not found.');
+                }),
+
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
@@ -83,7 +94,7 @@ class BarcodesResource extends Resource
     {
         return [
             'index' => Pages\ListBarcodes::route('/'),
-            'create' => Pages\CreateBarcodes::route('/create'),
+            'create' => Pages\CreateQr::route('/create'),
             'edit' => Pages\EditBarcodes::route('/{record}/edit'),
         ];
     }
