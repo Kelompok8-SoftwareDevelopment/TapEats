218419ceb053c12e295987c71e281f2f98022873
jclynviaa <jocelyn.535230046@stu.untar.ac.id>
2025-05-21 23:03:28 +0700

    ganti logo di admin dashboard

--- tapeats/app/Providers/Filament/AdminPanelProvider.php
+++ tapeats/app/Providers/Filament/AdminPanelProvider.php
@@ -30,6 +30,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->favicon(asset('favicon.svg')) 
             ->colors([
                 'danger' => Color::Rose,
                 'gray' => Color::Zinc,
